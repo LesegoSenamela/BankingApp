6b6b63932209f0290b7045c45898297eef24b3fb
LesegoSenamela <151392368+LesegoSenamela@users.noreply.github.com>
2025-06-27 12:00:41 +0200

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -2,95 +2,143 @@
 
 using namespace std;
 
-struct account{
+class account{
+private:
+
     int accountNumber;
     double accountBalance;
-    double interestRate;
+    const static double interestRate ; //3%
+
+public:
+
+    void enterAccountData();
+    double computeInterest(int);
+    void displayAccount();
+    int getAccountNumber();
+    void setAccountNumber(int);
+    double getAccountBalance();
+    void setAccountBalance(double);
+
 };
 
+    const double account::interestRate = 0.03;  // Definition outside the class for a constant static variable
 
-account enterAccountData(){
-    account BankAccount;
+    int account::getAccountNumber(){
+        return accountNumber;
+    }
 
-    cout<< "\nEnter the account number : ";
-    cin>> BankAccount.accountNumber;
+    void account::setAccountNumber(int accNo){
+        accountNumber = accNo;
+    }
 
-    while (BankAccount.accountNumber < 1000 || BankAccount.accountNumber > 9999){
-        cout<<"\nInvalid Account Number, An account number must be a 4-digit number. Please Try Again. ";
+    double account::getAccountBalance(){
+        return accountBalance;
+    }
+
+    void account::setAccountBalance(double accBal){
+        accountBalance = accBal;
+    }
+
+    void account::enterAccountData(){
+    // best practice is to prompt in main then use setters
         cout<< "\nEnter the account number : ";
-        cin>> BankAccount.accountNumber;
-    }
+        cin>> accountNumber;
 
-    cout<< "\nEnter the account starting balance : ";
-    cin>> BankAccount.accountBalance;
+        while (accountNumber < 1000 || accountNumber > 9999){
+            cout<<"\nInvalid Account Number, An account number must be a 4-digit number. Please Try Again. ";
+            cout<< "\nEnter the account number : ";
+            cin>> accountNumber;
+        }
 
-    while (BankAccount.accountBalance < 0 || BankAccount.accountBalance > 100000){
-        cout<<"\nInvalid Account Balance, Balance cannot be a negative value or above 100 000. Please Try Again.";
         cout<< "\nEnter the account starting balance : ";
-        cin>> BankAccount.accountBalance;
+        cin>> accountBalance;
+
+        while (accountBalance < 0 || accountBalance > 100000){
+            cout<<"\nInvalid Account Balance, Balance cannot be a negative value or above 100 000. Please Try Again.";
+            cout<< "\nEnter the account starting balance : ";
+            cin>> accountBalance;
+        }
+
     }
 
-    cout<< "\nEnter the account's interest rate : ";
-    cin>> BankAccount.interestRate;
+    double account::computeInterest(int yearsofInterest){
 
-    while(BankAccount.interestRate < 0 || BankAccount.interestRate > 0.15){
-        cout<< "\nInvalid Interest Rate, Interest rate must be between 0 and 0.15. Please try again.";
-        cout<< "\nEnter the account's interest rate : ";
-        cin>> BankAccount.interestRate;
+        double depositAmt, withdrawalAmt ;
+
+        cout<< "\nEnter the account's deposit amount : ";
+        cin>> depositAmt; //same for both accounts
+
+        cout<< "\nEnter the account's withdrawal amount : ";
+        cin>> withdrawalAmt; //same for both accounts
+
+        for (int i = 0; i < yearsofInterest; i++){
+
+            double intrestEarned = (0.83 * interestRate) * accountBalance * i;
+            accountBalance = accountBalance + intrestEarned + depositAmt - withdrawalAmt ;
+
+            cout<<"\nInterest for year "<<i<<" is : R"<<accountBalance;
+        }
+
+        return accountBalance;
     }
 
-    return BankAccount;
-}
+    void account::displayAccount(){
 
-account computeInterest(account bankAccount){
-
-    double depositAmt, withdrawalAmt ;
-    int holdingTerm;
-
-    cout<< "\nEnter the account's holding term : ";
-    cin>> holdingTerm; //same for both accounts
-
-    while (holdingTerm < 0 || holdingTerm > 10){
-    cout<< "\nInvalid Input! The account term must be between 0 and 10 years.\nEnter the account's holding term : ";
-    cin>> holdingTerm;
+        cout<<"\n*************Account Information**************"<<endl;
+        cout<<"Account Number : "<<accountNumber<<endl;
+        cout<<"Balance : "<<accountBalance<<endl;
+        cout<<"Interest Rate : "<<interestRate<<endl;
     }
 
-    cout<< "\nEnter the account's deposit amount : ";
-    cin>> depositAmt; //same for both accounts
 
-    cout<< "\nEnter the account's withdrawal amount : ";
-    cin>> withdrawalAmt; //same for both accounts
-
-    double intrestEarned = (0.83 * bankAccount.interestRate) * bankAccount.accountBalance;
-    bankAccount.accountBalance += ((intrestEarned + depositAmt) - withdrawalAmt);
-
-    return bankAccount;
-}
-
-void displayAccount(account accountData){
-
-    cout<<"\n*************Account Information**************"<<endl;
-    cout<<"Account Number : "<<accountData.accountNumber<<endl;
-    cout<<"Balance : "<<accountData.accountBalance<<endl;
-    cout<<"Interest Rate : "<<accountData.interestRate<<endl;
-}
 
 int main(){
 
-    account accountData[5];
+    int arrLength = 10;
+    account accountData[arrLength];
     double transferAmount;
-    int checkDigit;
+    int checkDigit, holdingTerm;
+    char input;
 
-    for (int i = 0; i < 5; i++){
-        accountData[i] = enterAccountData();
-        checkDigit = accountData[i].accountNumber % 5;
-        accountData[i].accountNumber = (accountData[i].accountNumber * 10) + checkDigit;//account number plus the check digit
-        accountData[i] = computeInterest(accountData[i]);
+    for (int i = 0; i < arrLength; i++){
+        accountData[i].enterAccountData();
+        checkDigit = accountData[i].getAccountNumber() % 5;
+        accountData[i].setAccountNumber((accountData[i].getAccountNumber() * 10) + checkDigit);//account number plus the check digit
 
-        if(accountData[i].accountNumber == accountData[i+1].accountNumber){
+        cout<<"\nDo you want to continue ? Y\\N >>";
+        cin>>input;
+
+        input = toupper(input);
+
+        if(input == 'Y')
+            continue;
+        else{
+            if(input == 'N')
+                i = arrLength;
+            else{
+                cout<<"Invalid Input. Stopping ....";
+                i = arrLength;
+            }
+        }
+
+    };
+
+    cout<< "\nEnter the account's holding term : ";
+    cin>> holdingTerm; //same for all accounts
+    while (holdingTerm < 0 || holdingTerm > 10){
+        cout<< "\nInvalid Input! The account term must be between 0 and 10 years.\nEnter the account's holding term : ";
+        cin>> holdingTerm;
+    }
+
+
+    for (int i = 0; i < arrLength; i++){
+
+        accountData[i].computeInterest(holdingTerm);
+
+        if(accountData[i].getAccountNumber() == accountData[i+1].getAccountNumber()){
             cout<<"\n\nThe Account Numbers Are The Same. Failed To Do Operation.";
         //output metrics
-            displayAccount(accountData[i]);
+            accountData[i].displayAccount();
             cout<< "\nCheck Digit : " << checkDigit <<endl;
         }
 
@@ -100,38 +148,39 @@ int main(){
             cout<< "\nEnter the transfer amount : ";
             cin>> transferAmount;
 
-            accountData[i].accountBalance -= transferAmount;//transferred to account i++
-            cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].accountBalance;
+            accountData[i].setAccountBalance(accountData[i].getAccountBalance() - transferAmount);//transferred to account i++
+            cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].getAccountBalance();
 
-            if (accountData[i].accountBalance - transferAmount <= 0){
-                cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].accountBalance;
-                if(accountData[i+1].accountBalance > 100000)
+            if (accountData[i].getAccountBalance() <= 0){
+                cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].getAccountBalance();
+                if(accountData[i+1].getAccountBalance() > 100000)
                     cout<<"\nWarning! Transfered Account Balance Above 100 000 ";
 
-                displayAccount(accountData[i+1]);
-                cout<< "\nBalance(After Transfer,Second Account) : "<< accountData[i+1].accountBalance;
+                accountData[i+1].displayAccount();
+                cout<< "\nBalance(After Transfer,Second Account) : "<< accountData[i+1].getAccountBalance();
                 }
             else{//none negative balance after transfer
-                if(accountData[i].accountBalance < 10)
+                if(accountData[i].getAccountBalance() < 10)
                     cout<< "\nWarning! Account Below R10. ";
 
-                cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].accountBalance;
+                cout<< "\nBalance(After Transfer, First Account) : "<< accountData[i].getAccountBalance();
 
-                displayAccount(accountData[i]);
-                accountData[i+1].accountBalance += transferAmount;
+                accountData[i].displayAccount();
 
-                if(accountData[i+1].accountBalance > 100000)
+                accountData[i + 1].setAccountBalance(accountData[i + 1].getAccountBalance() + transferAmount);
+                //accountData[i+1].accountBalance += transferAmount;
+
+                if(accountData[i+1].getAccountBalance() > 100000)
                 cout<<"\nWarning! Account Balance Above 100 000 ";
-
-            cout<< "\nBalance(After Transfer, Second Account) : "<< accountData[i+1].accountBalance;
+                //you need to add the transferred amount to the second account, please change that.
+            cout<< "\nBalance(After Transfer, Second Account) : "<< accountData[i+1].getAccountBalance();
         }
     }
     };
     // display a table
-    for(int j = 0; j < 5; j++ ){
+    for(int j = 0; j < arrLength; j++ ){
         cout<<"\n\n\n***********Final Account Summary***********";
-        displayAccount(accountData[j]);
+        accountData[j].displayAccount();
     }
     return 0;
 }
-
